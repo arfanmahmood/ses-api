02df9a43bb82cffb9b0d9d897d3d98ebd6c28967
Arfan Mahmood <arfan.mahmood@hotmail.com>
2013-09-04 18:29:08 +0500

    Adding Mailer File
    Adding mailer file with basic form to send emails.

--- mailer.php
+++ mailer.php
@@ -1,3 +1,27 @@
+<?php
+if($_REQUEST['hdnSubmit']){ /* Checking if form submitted */
+
+  /* Initilizing SES Class */	
+  require_once('ses.php');
+  $ses = new SimpleEmailService('Access Key Here', 'Secret Key Here');
+  
+  /* Script to Verify Email Address */
+  //print_r($ses->verifyEmailAddress('dev@zeewebtech.com'));
+  
+  /* Script to Check Verified Email Addresses List */
+  //print_r($ses->listVerifiedEmailAddresses());
+  
+  /* Initilizing Final SES Class */
+  $m = new SimpleEmailServiceMessage();
+  $m->addTo($_REQUEST['email']);
+  $m->setFrom('donotreply@rembrand.ca');
+  $m->setSubject($_REQUEST['subject']);
+  $m->setMessageFromString($_REQUEST['message']);
+  
+  print_r($ses->sendEmail($m));
+  
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -6,9 +30,13 @@
 </head>
 
 <body>
-<?php
-require_once('ses.php');
-$ses = new SimpleEmailService('Access Key Here', 'Secret Key Here');
-?>
+<!-- Simple Email Form to Send -->
+<form action="" method="post" name="email">
+  <label>Email:<br /><input type="text" name="email" value="" /></label><br /><br />
+  <label>Subject:<br /><input type="text" name="subject" value="" /></label><br /><br />
+  <label>Message:<br /><textarea name="message" cols="50" rows="5"></textarea></label><br /><br />
+  <input type="submit" value="Submit" />
+  <input type="hidden" name="hdnSubmit" value="1">
+</form>
 </body>
 </html>

879871e27abf6329cda6807c0f3cb50fe836c1bc
arfanmahmood <arfan.mahmood@hotmail.com>
2013-09-25 15:31:33 +0500

    CSV Option
    Updating mailer.php file and adding a sample CSV file.

--- mailer.php
+++ mailer.php
@@ -1,27 +1,3 @@
-<?php
-if($_REQUEST['hdnSubmit']){ /* Checking if form submitted */
-
-  /* Initilizing SES Class */	
-  require_once('ses.php');
-  $ses = new SimpleEmailService('Access Key Here', 'Secret Key Here');
-  
-  /* Script to Verify Email Address */
-  //print_r($ses->verifyEmailAddress('dev@zeewebtech.com'));
-  
-  /* Script to Check Verified Email Addresses List */
-  //print_r($ses->listVerifiedEmailAddresses());
-  
-  /* Initilizing Final SES Class */
-  $m = new SimpleEmailServiceMessage();
-  $m->addTo($_REQUEST['email']);
-  $m->setFrom('donotreply@rembrand.ca');
-  $m->setSubject($_REQUEST['subject']);
-  $m->setMessageFromString($_REQUEST['message']);
-  
-  print_r($ses->sendEmail($m));
-  
-}
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -30,29 +6,44 @@ if($_REQUEST['hdnSubmit']){ /* Checking if form submitted */
 </head>
 
 <body>
-<!-- Simple Email Form to Send -->
 <?php
+  /* Initilizing SES Class */	
+  require_once('ses.php');
+  $ses = new SimpleEmailService('Access Key Here', 'Secret Key Here');
+  
+  /* Going to define variables for subject and message */
+  $subject = "Real Estate Branding and Marketing by REMbrand";
+  $message = file_get_contents('http://www.rembrand.ca/resources/rembrand/newsletter/dec-2012/rembrand-newsletter.html');
+  $message = str_replace("{date}", date("F j, Y"), $message);
+  
 if (($handle = fopen("file.csv", "r")) !== FALSE) {
+	
+	$row = 0;
 	
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 		
-        for ($c=0; $c < count($data); $c++) {
-            echo $data[$c].' ';
-        }
-		
-		echo "<br />\n";
+		if($row==0){
+			
+			$name = $data[0];
+			$email = $data[1];
+			$message = str_replace("{name}", $name, $message);
+  			$message = str_replace("{email}", $email, $message);
+
+			/* Initilizing Final SES Class */
+			$m = new SimpleEmailServiceMessage();
+			$m->addTo($_REQUEST['email']);
+			$m->setFrom('donotreply@rembrand.ca');
+			$m->setSubject($subject);
+			$m->setMessageFromString(null,$message);
+			
+			print_r($ses->sendEmail($m));
+			
+		}
 		
     }
     fclose($handle);
 }
+  
 ?>
-
-<form action="" method="post" name="email">
-  <label>Email:<br /><input type="text" name="email" value="" /></label><br /><br />
-  <label>Subject:<br /><input type="text" name="subject" value="" /></label><br /><br />
-  <label>Message:<br /><textarea name="message" cols="50" rows="5"></textarea></label><br /><br />
-  <input type="submit" value="Submit" />
-  <input type="hidden" name="hdnSubmit" value="1">
-</form>
 </body>
 </html>
